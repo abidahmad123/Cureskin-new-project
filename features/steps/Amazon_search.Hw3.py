69c6ea6b2e66458480d78f6dcbe37605403a3ea5
abidahmad123 <ahmadabid076@gmail.com>
2023-05-30 19:33:55 -0700

    homework 4

--- features/steps/Amazon_search.Hw3.py
+++ features/steps/Amazon_search.Hw3.py
@@ -3,18 +3,18 @@ from behave import given, when, then
 from time import sleep
 
 
-@given('Open Amazon main page')
-def open_amazon(context):
-    context.driver.get('https://www.amazon.com/')
+@given('Open Best Sellers page')
+def open_best_sellers(context):
+    context.driver.get('https://www.amazon.com/Best-Sellers/zgbs')
 
 
-@when('Search for Best Sellers')
-def search_amazon(context):
-    context.driver.find_element(By.ID, 'nav_cs_bestsellers').click()
+#@when('Search for Best Sellers')
+#def search_amazon(context):
+    #context.driver.find_element(By.ID, 'nav_cs_bestsellers').click()
 
 
 @then('Verify search results shown')
 def verify_search_results(context):
     expected_results = 'Amazon Best Sellers'
-    actual_result = context.driver.find_element(By.CSS_SELECTOR, 'a-size-extra-large a-color-base _p13n-zg-banner-landing-page-header_style_zgLandingPageBannerText__3HlJo')
+    actual_result = context.driver.find_element(By.CSS_SELECTOR, '#zg_banner_text').text
     assert expected_results == actual_result, f'Expected {expected_results} but got {actual_result}'
--- features/tests/Amazon_search.hw3.feature
+++ features/tests/Amazon_search.hw3.feature
@@ -2,9 +2,9 @@
 Feature: Amazon Search tests
 
 
-  Scenario: user can search on Amazon
-    Given Open Amazon main page
-    When Search for Best Sellers
+  Scenario: User can search on Amazon
+    Given Open Best Sellers page
+    #When Search for Best Sellers
     Then Verify search results shown
 
 
